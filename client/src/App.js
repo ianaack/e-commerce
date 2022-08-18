fb0f5d7484175eba5dd994a98dcf6122d6544e90
ianaack <ianaack@gmail.com>
2022-08-17 23:17:36 -0400

    npm run develop working

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import "bootstrap";
+
+// import pages
+import Home from "./pages/Home";
+import Detail from "./pages/Detail";
+
+// import components
+// import AddProduct from "./components/addProduct";
+// import UpdateProduct from "./components/updateProduct";
+// import ViewProducts from "./components/viewProducts";
+// import RemoveProduct from "./components/removeProduct";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	<Router>
+		<Routes>
+			<nav className="navbar navbar-expand navbar-dark bg-secondary">
+				<Link
+					to={"/products"}
+					className="navbar-brand"
+					style={{ paddingLeft: "5%" }}
+				>
+					Products
+				</Link>
+			</nav>
+			<Route path="/" element={<Home />} />
+			<Route path="/products/:id" element={<Detail />} />
+		</Routes>
+	</Router>;
 }
 
 export default App;
